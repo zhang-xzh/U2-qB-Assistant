3dde1a7b8a80fe09c2d9e755c6d16e80b9a1bcab
HRSKNK <zhangxzh.pro@gmail.com>
2026-02-17 12:02:32 +0800

    refactor: remove unused files and code, streamline project structure

--- src/background/api.js
+++
@@ -1,9 +0,0 @@
-import { fetchTextOrThrow } from '../utils';
-export const sendOk = (sendResponse, data) => sendResponse({ success: true, data });
-export const sendError = (sendResponse, error) => sendResponse({ success: false, error: error instanceof Error ? error.message : String(error) });
-export const handleQBAPI = (message, sendResponse) => {
-    fetchTextOrThrow(message.url, message.options)
-        .then((text) => sendOk(sendResponse, text))
-        .catch((err) => sendError(sendResponse, err));
-    return true;
-};
